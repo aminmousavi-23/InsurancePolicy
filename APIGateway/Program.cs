236357b9fd322fec3bc8a11615e652d0bb59a094
amin mousavi <mousavia54@gmail.com>
2024-10-31 19:25:34 +0330

    Init APIGateway and inject UIserService into that

---
+++ APIGateway/Program.cs
@@ -0,0 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
